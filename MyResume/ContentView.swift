917e2ab0d47082243a99c45554f2008d29d26b88
liyoway <liyoway@gmail.com>
2024-01-25 16:34:33 +0800

    Initial Commit

---
+++ MyResume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyResume
+//
+//  Created by yoway Li on 2024/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyResume/MyResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyResumeApp.swift
+//  MyResume
+//
+//  Created by yoway Li on 2024/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MyResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
